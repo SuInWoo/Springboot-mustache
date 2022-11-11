8d9be041726149734f9c1ec58b7d7ef7e4996b67
SuInWoo <wbo1026@naver.com>
2022-11-11 13:52:28 +0900

    SpringBoot Mustache Hospital - paging

--- src/main/java/com/mustache/bbs/controller/HospitalController.java
+++ src/main/java/com/mustache/bbs/controller/HospitalController.java
@@ -5,6 +5,8 @@ import com.mustache.bbs.repository.HospitalRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +24,7 @@ public class HospitalController {
     }
 
     @GetMapping("")
-    public String list(Model model, Pageable pageable) {
+    public String list(Model model, @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
         Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
         log.info("size:{}", hospitals.getSize());
         model.addAttribute("hospitals", hospitals);
