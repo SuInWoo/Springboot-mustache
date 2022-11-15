87069d45f098a46586d1add389f18e54035e1485
SuInWoo <wbo1026@naver.com>
2022-11-15 15:01:24 +0900

    SpringBoot Hospital - insert Service

--- src/main/java/com/mustache/bbs/controller/HospitalRestController.java
+++ src/main/java/com/mustache/bbs/controller/HospitalRestController.java
@@ -1,30 +1,27 @@
 package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.HospitalResponse;
-import com.mustache.bbs.domain.entity.Hospital;
-import com.mustache.bbs.repository.HospitalRepository;
+import com.mustache.bbs.service.HospitalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/hospitals")
 public class HospitalRestController {
 
-    private final HospitalRepository hospitalRepository;
+    private final HospitalService hospitalService;
 
-    public HospitalRestController(HospitalRepository hospitalRepository) {
-        this.hospitalRepository = hospitalRepository;
+    public HospitalRestController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) { //ResponseEntity도 DTO타입
-        Optional<Hospital> hospital = hospitalRepository.findById(id);  //Entity
-        HospitalResponse hospitalResponse = Hospital.of(hospital.get());    //DTO
-        return ResponseEntity.ok().body(hospitalResponse);  //return은 DTO로
+    public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) {
+        HospitalResponse hospitalResponse = hospitalService.getHospital(id); //DTO
+        return ResponseEntity.ok().body(hospitalResponse);
     }
 }
